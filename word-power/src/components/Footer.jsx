87e30652876a9baae8c5cfbf04ef08c2c646449b
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-05-24 13:38:34 -0700

    Project 12: 5-24-24

---
+++ word-power/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+ function Footer() {
+    return (
+        <footer>
+            <a href="#">Purchase the premium version</a> of Vocab Builder, which contains many more words — we promise!
+        </footer>
+    )
+}
+
+export default Footer
---
+++ word-power/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+ function Header() {
+    return (
+        <header>
+            <h1>📚 Word Power 📚</h1>
+        </header>
+    )
+}
+
+export default Header
