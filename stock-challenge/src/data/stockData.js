ab150d58f92fe7d493dab8166ec84da0e63bcf5a
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-05-27 10:45:14 -0700

    added remove button: 5-27-24

--- stock-challenge/src/data/stockData.js
+++ stock-challenge/src/data/stockData.js
@@ -1,27 +1,27 @@
 export default [
     {
         stockName: "AAPL", 
-        logo: "./images/apple.svg",
+        logo: "./assets/images/apple.svg",
         currentPrice: 153.83,
         prevClosingPrice: 151.03 
     },{
         stockName: "META",
-        logo: "./images/meta.svg",
+        logo: "./assets/images/meta.svg",
         currentPrice: 184.91,
         prevClosingPrice: 185.25
     },{
         stockName: "GOOGL",
-        logo: "./images/google.svg", 
+        logo: "./assets/images/google.svg", 
         currentPrice: 95.13,
         prevClosingPrice: 92.65
     },{
         stockName: "AMZN", 
-        logo: "./images/amazon.svg",
+        logo: "./assets/images/amazon.svg",
         currentPrice: 93.75,
         prevClosingPrice: 94.91
     },{
         stockName: "MSFT",
-        logo: "./images/microsoft.svg", 
+        logo: "./assets/images/microsoft.svg", 
         currentPrice: 256.85,
         prevClosingPrice: 256.85 
     }
