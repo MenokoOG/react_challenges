2a97150beb7056376307d80fd782e8ed1258c638
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-07-02 17:22:38 -0700

    imagedatafile imports: 7-2-24

--- antimotivational/src/data/imageData.js
+++ antimotivational/src/data/imageData.js
@@ -1,313 +1,295 @@
+import austinSchmid from '../images/austin-schmid-hRdVSYpffas-unsplash.jpg';
+import camAdams from '../images/cam-adams-imBSxksI7DA-unsplash.jpg';
+import jeremyPerkins from '../images/jeremy-perkins-7S1yZoFcVV0-unsplash.jpg';
+import joshuaEarle from '../images/joshua-earle-9idqIGrLuTE-unsplash.jpg';
+import joshuaSortino from '../images/joshua-sortino-1Y7zSILdde4-unsplash.jpg';
+import madaraParma from '../images/madara-parma-K1_hvL9yUBk-unsplash.jpg';
+import nathanShipps from '../images/nathan-shipps-ke2-WbTxINI-unsplash.jpg';
+import johannesPlenio from '../images/johannes-plenio-DKix6Un55mw-unsplash.jpg';
+import shaneRounce from '../images/shane-rounce-08M0vQW3eMw-unsplash.jpg';
+import simonBerger from '../images/simon-berger-twukN12EN7c-unsplash.jpg';
+import brookeLark from '../images/brooke-lark-ZOFyElHZIlM-unsplash.jpg';
+import syedHadiNaqvi from '../images/syed-hadi-naqvi-DzyrFjJbLMg-unsplash.jpg';
+import xanGriffin from '../images/xan-griffin-eA2t5EvcxU4-unsplash.jpg';
+import gregRakozy from '../images/greg-rakozy-oMpAz-DN-9I-unsplash.jpg';
+import johnsonWang from '../images/johnson-wang-4flhKx1sUdE-unsplash.jpg';
+import rahulBhosale from '../images/rahul-bhosale-yBgC-qVCxMg-unsplash.jpg';
+import aaronLee from '../images/aaron-lee-hXcGZg3rAkU-unsplash.jpg';
+import joshuaEarle2 from '../images/joshua-earle-C6duwascOEA-unsplash.jpg';
+
 export default {
-	availableItems: [
-		
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/austin-schmid-hRdVSYpffas-unsplash.jpg)",
-				backgroundPositionX: "50%",
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: "90%",
-				padding: "30px 50px 0 40px",
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "40px"
-			}
-		},
+  availableItems: [
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${austinSchmid})`,
+        backgroundPositionX: '50%',
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: '90%',
+        padding: '30px 50px 0 40px',
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '40px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${camAdams})`,
+        backgroundPositionX: '0%',
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${jeremyPerkins})`,
+        backgroundPositionX: '80%',
+      },
+      containerStyles: {
+        color: '#252422',
+        width: '85%',
+        padding: '50px 100px 50px 50px',
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '30px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${joshuaEarle})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${joshuaSortino})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '26px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${madaraParma})`,
+        backgroundPositionX: '30%',
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '24px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${nathanShipps})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: '50px 10px 0px 80px',
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${johannesPlenio})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '35px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${shaneRounce})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: '130px 20px',
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '25px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${simonBerger})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${brookeLark})`,
+        backgroundPositionX: '30%',
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '30px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${syedHadiNaqvi})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${xanGriffin})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: '300px 50px 0 50px',
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '20px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${gregRakozy})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${johnsonWang})`,
+        backgroundPositionX: '70%',
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${rahulBhosale})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '30px',
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${aaronLee})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#252422',
+        width: undefined,
+        padding: undefined,
+        textShadow: '0 3px 5px white',
+      },
+      quoteFontSize: {
+        fontSize: undefined,
+      },
+    },
+    {
+      wrapperStyles: {
+        backgroundImage: `url(${joshuaEarle2})`,
+        backgroundPositionX: undefined,
+      },
+      containerStyles: {
+        color: '#f8f8ff',
+        width: undefined,
+        padding: undefined,
+        textShadow: undefined,
+      },
+      quoteFontSize: {
+        fontSize: '40px',
+      },
+    },
+  ],
 
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/cam-adams-imBSxksI7DA-unsplash.jpg)",
-				backgroundPositionX: "0%",
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-		{
-			wrapperStyles: {
-				backgroundImage:
-					"url(./images/jeremy-perkins-7S1yZoFcVV0-unsplash.jpg)",
-				backgroundPositionX: "80%",
-			},
-			containerStyles: {
-				color: "#252422",
-				width: "85%",
-				padding: "50px 100px 50px 50px",
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "30px"
-			}
-		},
-		
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/joshua-earle-9idqIGrLuTE-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage:
-					"url(./images/joshua-sortino-1Y7zSILdde4-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "26px"
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/madara-parma-K1_hvL9yUBk-unsplash.jpg)",
-				backgroundPositionX: "30%",
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "24px"
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/nathan-shipps-ke2-WbTxINI-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: "50px 10px 0px 80px",
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage:
-					"url(./images/johannes-plenio-DKix6Un55mw-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "35px"
-			},
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/shane-rounce-08M0vQW3eMw-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: "130px 20px",
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "25px"
-			},
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/simon-berger-twukN12EN7c-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/brooke-lark-ZOFyElHZIlM-unsplash.jpg)",
-				backgroundPositionX: "30%",
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "30px"
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage:
-					"url(./images/syed-hadi-naqvi-DzyrFjJbLMg-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/xan-griffin-eA2t5EvcxU4-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: "300px 50px 0 50px",
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "20px"
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/greg-rakozy-oMpAz-DN-9I-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-				quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/johnson-wang-4flhKx1sUdE-unsplash.jpg)",
-				backgroundPositionX: "70%",
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/rahul-bhosale-yBgC-qVCxMg-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "30px"
-			},
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/aaron-lee-hXcGZg3rAkU-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#252422",
-				width: undefined,
-				padding: undefined,
-				textShadow: "0 3px 5px white",
-			},
-			quoteFontSize: {
-				fontSize: undefined
-			}
-		},
-
-
-		{
-			wrapperStyles: {
-				backgroundImage: "url(./images/joshua-earle-C6duwascOEA-unsplash.jpg)",
-				backgroundPositionX: undefined
-			},
-			containerStyles: {
-				color: "#f8f8ff",
-				width: undefined,
-				padding: undefined,
-				textShadow: undefined
-			},
-			quoteFontSize: {
-				fontSize: "40px"
-			}
-		},
-	],
-
-	usedItems: [],
-}
+  usedItems: [],
+};
